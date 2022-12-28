264e298b22b4af2fa094bc4346bf8c8c64aca14c
SolutionKhow <Ansh1305@outlook.com>
2022-12-28 12:12:20 +0530

    first commit

--- promise.js
+++ promise.js
@@ -63,22 +63,59 @@ const stocks = {
 //     console.log(`${stocks.Fruits[1]}`);
 // });
 
-const Is_Shop_Open=true;
+const Is_Shop_Open = true;
 
-const Order=(Time,Work)=>{
-   return new Promise((resolve,reject)=>{
-    if(Is_Shop_Open){
-        setTimeout(()=>{
-            resolve(Work());
+const Order = (Time, Work) => {
+    return new Promise((resolve, reject) => {
+        if (Is_Shop_Open) {
+            setTimeout(() => {
+                resolve(Work());
 
-        },Time);
-    }
-    else{
-        reject("Order is rejected");
-    }
+            }, Time);
+        }
+        else {
+            reject("Order is rejected");
+        }
 
-   });
+    });
 }
-Order(2000,()=>{
-    console.log(`${stocks.Holder[1]}`);
-});
+Order(2000, () => {
+    console.log(`${stocks.Fruits[1]} was selected`);
+})
+    //Now we put Promise Chainig Sequence Here...
+    .then(()=>{
+        return Order(0000,()=>{
+            console.log('Production is started');
+        });
+    })
+    .then(()=>{
+        return Order(2000,()=>{
+            console.log('Food was Chopped');
+        })
+    })
+    .then(()=>{
+        return Order(1000,()=>{
+            console.log(`Add ${stocks.Liquid[0]} and ${stocks.Liquid[1]} `)
+        });
+    })
+    .then(()=>{
+        return Order(1000,()=>{
+            console.log('Start Machine');
+
+        })
+    })
+    .then(()=>{
+        return Order(2000,()=>{
+            console.log(`${stocks.Holder[1]} for Ice Cream Hoding `);
+        });
+    })
+    .then(()=>{
+        return Order(3000,()=>{
+            console.log(`${stocks.Topping[1]} is selected for Toppind`)
+        })
+    })
+    .then(()=>{
+        return Order(2000,()=>{
+            console.log('Server the ICe Cream');
+        });
+    });
